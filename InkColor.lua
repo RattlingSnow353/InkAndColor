03ae4300610d012ea38435eea80ee50a75c3e7c7
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2024-09-03 21:46:17 -0800

    Add files via upload

--- InkColor.lua
+++ InkColor.lua
@@ -255,24 +255,28 @@ SMODS.Consumable {
         for i=1, #G.hand.highlighted do
             local percent = 1.15 - (i-0.999)/(#G.hand.highlighted-0.998)*0.3
             G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.15, func = function()
-                G.hand.highlighted[i]:flip();play_sound('card1', percent);G.hand.highlighted[i]:juice_up(0.3, 0.3);
-            return true end })
+                G.hand.highlighted[i]:flip()
+                play_sound('card1', percent)
+                G.hand.highlighted[i]:juice_up(0.3, 0.3)
+            return true end }))
         end
         delay(0.2)
         for i=1, #G.hand.highlighted do
             G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.1, func = function()
-                G.hand.highlighted[i]:change_suit(self.config.suit_conv);
-            return true end })
+                G.hand.highlighted[i]:change_suit(self.config.suit_conv)
+            return true end }))
         end
         for i=1, #G.hand.highlighted do
             local percent = 0.85 + ( i - 0.999 ) / ( #G.hand.highlighted - 0.998 ) * 0.3
             G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.15, func = function()
-                G.hand.highlighted[i]:flip(); play_sound('tarot2', percent, 0.6); G.hand.highlighted[i]:juice_up(0.3, 0.3);
-            return true end })
+                G.hand.highlighted[i]:flip()
+                play_sound('tarot2', percent, 0.6)
+                G.hand.highlighted[i]:juice_up(0.3, 0.3);
+            return true end }))
         end
         G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.2, func = function()
             G.hand:unhighlight_all();
-        return true end })
+        return true end }))
         delay(0.5)
     end,
 }
@@ -297,24 +301,28 @@ SMODS.Consumable {
         for i=1, #G.hand.highlighted do
             local percent = 1.15 - (i-0.999)/(#G.hand.highlighted-0.998)*0.3
             G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.15, func = function()
-                G.hand.highlighted[i]:flip();play_sound('card1', percent);G.hand.highlighted[i]:juice_up(0.3, 0.3);
-            return true end })
+                G.hand.highlighted[i]:flip()
+                play_sound('card1', percent)
+                G.hand.highlighted[i]:juice_up(0.3, 0.3)
+            return true end }))
         end
         delay(0.2)
         for i=1, #G.hand.highlighted do
             G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.1, func = function()
-                G.hand.highlighted[i]:change_suit(self.config.suit_conv);
-            return true end })
+                G.hand.highlighted[i]:change_suit(self.config.suit_conv)
+            return true end }))
         end
         for i=1, #G.hand.highlighted do
             local percent = 0.85 + ( i - 0.999 ) / ( #G.hand.highlighted - 0.998 ) * 0.3
             G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.15, func = function()
-                G.hand.highlighted[i]:flip(); play_sound('tarot2', percent, 0.6); G.hand.highlighted[i]:juice_up(0.3, 0.3);
-            return true end })
+                G.hand.highlighted[i]:flip()
+                play_sound('tarot2', percent, 0.6)
+                G.hand.highlighted[i]:juice_up(0.3, 0.3);
+            return true end }))
         end
         G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.2, func = function()
             G.hand:unhighlight_all();
-        return true end })
+        return true end }))
         delay(0.5)
     end,
 }
